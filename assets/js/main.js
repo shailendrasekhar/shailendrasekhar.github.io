da63e80e85aeba75a5c136af0104f43f42ea7ed8
shailendrasekhar <shailendrasekhar@gmail.com>
2025-03-15 21:21:33 +0530

    Making it mobile friendly

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,33 +156,28 @@
 
   // Initialize mobile navigation
   function initMobileNav() {
-    if ($('.nav-menu').length) {
-      var $mobile_nav = $('.nav-menu').clone().prop({
-        class: 'mobile-nav d-lg-none'
-      });
-      
-      $('body').append($mobile_nav);
-      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-      
-      // Toggle mobile nav
-      $(document).on('click', '.mobile-nav-toggle', function(e) {
-        $('body').toggleClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      });
+    // Toggle mobile nav
+    $(document).on('click', '.mobile-nav-toggle', function(e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('bx-menu bx-x');
+    });
 
-      // Close mobile nav when clicking outside
-      $(document).on('click', function(e) {
-        var container = $(".mobile-nav, .mobile-nav-toggle");
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          }
+    // Close mobile nav when clicking on links
+    $('.mobile-nav-menu a').on('click', function() {
+      $('body').removeClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').removeClass('bx-x').addClass('bx-menu');
+    });
+
+    // Close mobile nav when clicking outside
+    $(document).on('click', function(e) {
+      var container = $(".mobile-nav-menu, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').removeClass('bx-x').addClass('bx-menu');
         }
-      });
-    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-      $(".mobile-nav, .mobile-nav-toggle").hide();
-    }
+      }
+    });
   }
 
   // Handle window resize
